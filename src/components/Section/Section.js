f0b60b504a31be3b298ce06738b2e215c3d5a5b4
Bartosz Hryniewicz <bartoszhryniewicz@MacBook-Air-Bartosz.local>
2022-03-06 15:32:40 +0100

    add styles to landing page and seciton component

---
+++ src/components/Section/Section.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Wrapper, WidthWrapper } from './Section.styles';
+
+export const Section = props => {
+  return (
+    <Wrapper>
+      <WidthWrapper>
+        <div>
+          <h1>Section Title</h1>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae incidunt sunt ratione
+            eaque alias, voluptas, delectus dolor, non nulla qui neque in eligendi eum maxime. Magni
+            similique iste eligendi veniam deleniti repellat quasi maiores quaerat expedita vitae
+            facilis, minima ipsa!
+          </p>
+        </div>
+        <img src="" alt="" />
+      </WidthWrapper>
+    </Wrapper>
+  );
+};
---
+++ src/components/Section/Section.styles.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  height: 86vh;
+  background-color: green;
+`;
+
+export const WidthWrapper = styled.div`
+  width: 70%;
+  margin: 0 auto;
+`;
